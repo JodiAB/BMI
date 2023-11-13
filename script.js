189e5f5b0697a787bbf6975c66a159f7c4608839
JodiAB <jodiabrahams9@gmail.com>
2023-11-13 14:24:31 +0200

    BMI commit

--- script.js
+++ script.js
@@ -1,48 +1,42 @@
 var age = document.getElementById('age');
 var height = document.getElementById('height');
 var weight = document.getElementById('weight');
-var male = document.getElementById('m');
-var female = document.getElementById('f');
-// var form = document.createElement('form');
-let resultArea = document.querySelector('.comment');
-
-modalContent = document.querySelector(".modal-content");
-modalText = document.querySelector("#modalText");
+var male = document.getElementById('male');
+var female = document.getElementById('female');
+var resultArea = document.querySelector('.comment');
 var modal = document.getElementById("myModal");
+var modalText = document.querySelector("#modalText");
 var span = document.getElementsByClassName("close")[0];
 
-function calculate(){
-    if(age.value =='' || height.value =='' || weight.value =='' || (male.checked ==false && female.checked ==false)){
+function calculate() {
+    if (age.value == '' || height.value == '' || weight.value == '' || (male.checked == false && female.checked == false)) {
         modal.style.display = "block";
         modalText.innerHTML = `All fields are required`;
-
-    }else{
+    } else {
         countBmi();
     }
 }
 
-function countBmi(){
+function countBmi() {
     var p = [age.value, height.value, weight.value];
-    if(male.checked){
+    if (male.checked) {
         p.push("male");
-
-    }else if (female.checked){
+    } else if (female.checked) {
         p.push("female");
     }
 
-    var bmi = Number(p[2])/(Number(p[1])/100*Number(p[1])/100);
+    var bmi = Number(p[2]) / (Number(p[1]) / 100 * Number(p[1]) / 100);
 
     var result = '';
-    if(bmi<18.5){
+    if (bmi < 18.5) {
         result = 'Underweight';
-    }else if (18.5<=bmi&&bmi<=24.9){
+    } else if (18.5 <= bmi && bmi <= 24.9) {
         result = 'Healthy';
-
-    }else if(25<=bmi&&bmi<=29.9){
+    } else if (25 <= bmi && bmi <= 29.9) {
         result = 'Overweight';
-    }else if (30<=bmi&&bmi<=34.9){
+    } else if (30 <= bmi && bmi <= 34.9) {
         result = 'Obese';
-    }else if(35<=bmi){
+    } else if (35 <= bmi) {
         result = 'Extremely obese';
     }
 
@@ -51,12 +45,12 @@ function countBmi(){
     document.querySelector("#result").innerHTML = bmi.toFixed(2);
 }
 
-span.onclick = function(){
+span.onclick = function () {
     modal.style.display = "none";
 }
 
-window.onclick = function(event){
-    if(event.target == modal){
+window.onclick = function (event) {
+    if (event.target == modal) {
         modal.style.display = "none";
     }
-}
+};
